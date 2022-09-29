51a07e435598a9c2d341d2a97e33b87779278251
giokim12 <giokim1011@gmail.com>
2022-09-29 14:00:41 +0900

    종이자르기

--- baekjoon/b_2563.py
+++ baekjoon/b_2563.py
@@ -1,3 +1,19 @@
 bucket = [[0]*101 for _ in range(101)]
 
-tc = int(input())
+T = int(input())
+
+for tc in range(T):
+    x, y = map(int, input().split())
+
+    for i in range(10):
+        for j in range(10):
+            bucket[y+i][x+j] +=1
+
+
+area = 0
+for i in range(101):
+    for j in range(101):
+        if bucket[i][j] >0:
+            area +=1
+
+print(area)
