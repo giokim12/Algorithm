017da4f55dce4b26693a1b80816f873e87da0a67
giokim <giokim1011@gmail.com>
2022-10-29 06:17:27 +0900

    .

---
+++ baekjoon/b_1991.py
@@ -0,0 +1,55 @@
+# nodes = int(input())
+# arr = [map(int, input().split()) for _ in range(nodes)]
+#
+# def rcr(level):
+# '''
+# A > B (a에서 1번) > D (b에서 1번) > . . (return)
+# 인덱스 0인거에서 인덱스 1인걸로 가라
+# 근데 인덱스 1이 .이면은 2로 가라
+# 둘다 .이면은 리턴
+#
+#
+# '''
+#     if sth:
+#         return
+#
+#
+#     print(arr[level][0], end='')
+#     rcr(level+1)
+import sys
+
+N = int(sys.stdin.readline().strip())
+tree = {}
+
+for n in range(N):
+    root, left, right = sys.stdin.readline().strip().split()
+    tree[root] = [left, right]
+
+
+def preorder(root):
+    if root != '.':
+        print(root, end='')  # root
+        preorder(tree[root][0])  # left
+        preorder(tree[root][1])  # right
+
+
+def inorder(root):
+    if root != '.':
+        inorder(tree[root][0])  # left
+        print(root, end='')  # root
+        inorder(tree[root][1])  # right
+
+
+def postorder(root):
+    if root != '.':
+        postorder(tree[root][0])  # left
+        postorder(tree[root][1])  # right
+        print(root, end='')  # root
+
+
+preorder('A')
+print()
+inorder('A')
+print()
+postorder('A')
+
