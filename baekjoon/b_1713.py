dd9f935008d3de85a7f29712a1e0eedb72f30240
giokim <giokim1011@gmail.com>
2022-12-20 23:24:04 +0900

    .

--- baekjoon/b_1713.py
+++ baekjoon/b_1713.py
@@ -1,8 +1,17 @@
-N = int(input())
-M = int(input())
+photo_frame_len = int(input())
+recommended_numbers = int(input())
 arr = list(map(int, input().split()))
-bucket = [0]*101
-for i in range(len(arr)):
-    bucket[arr[i]] += 1
+bucket = [0]*photo_frame_len
+photos = []
 
-print(bucket)
+for j in range(recommended_numbers):
+    if arr
+    # if bucket[arr[j]] == 0:
+#         bucket[arr[j]] += 1
+#         bucket[arr[j-photo_frame]] -=1
+#     elif bucket[arr[j]] > 0:
+#         bucket[arr[j]] += 1
+#
+# for i in range(len(bucket)):
+#     if bucket[i]>0:
+#         print(i, end=" ")
